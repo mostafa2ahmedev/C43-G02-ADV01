d34fd309fcbdde118937c94bf19b0486ed22d63c
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-01-30 04:45:15 +0200

    Q2 1)

---
+++ Demo/Range.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Range<T>
+    {
+      
+
+       
+    }
+}
